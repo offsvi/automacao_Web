d4759bf7b5af2f27cd4a8df790555e9a99c257c3
Vítor Oliveira <vitorsk807@gmail.com>
2023-02-27 22:56:52 -0300

    Inserindo link da página inicial do google

--- automatizandoSite.py
+++ automatizandoSite.py
@@ -5,7 +5,7 @@ chromedriver_path = "caminho/para/o/chromedriver"
 
 driver = webdriver.Chrome(executable_path=chromedriver_path)
 
-driver.get("https://www.google.com")
+driver.get("https://www.google.com") 
 
 search_box = driver.find_element_by_css_selector("input[name='q']")
 search_box.send_keys("Python automation" + Keys.RETURN)
